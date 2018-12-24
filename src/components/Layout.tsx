bd8eb03e96df5fec123c053b8290829953a46846
lucarge <luca.argenziano@icloud.com>
2018-12-24 21:23:27 +0100

    initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { rhythm, scale } from '../utils/typography'
+
+declare const __PATH_PREFIX__: string
+
+type Props = {
+  location: {
+    pathname: string
+  }
+  title?: string
+}
+
+const rootPath = `${__PATH_PREFIX__}/`
+
+const Layout: React.FunctionComponent<Props> = ({ children, location, title }) => {
+  if (location.pathname === rootPath) {
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+
+        {children}
+      </div>
+    )
+  }
+
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+          marginBottom: rhythm(-1),
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+
+      {children}
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
+
+type Props = {
+  location: {
+    pathname: string
+  }
+}
+
+const NotFoundPage: React.FunctionComponent<Props> = ({ location }) => (
+  <Layout location={location}>
+    <Seo title="404: Not Found" />
+    <h1>Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
+
+export default NotFoundPage
